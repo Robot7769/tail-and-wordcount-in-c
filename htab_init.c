418b027d3fc99bce29d3960dadbb5e50d052926e
Robot7769 <jan.robot7@gmail.com>
2022-04-17 23:26:24 +0200

    error -> warning

--- htab_init.c
+++ htab_init.c
@@ -15,13 +15,15 @@
 htab_t *htab_init(size_t n) {
     htab_t *tab = calloc(1, sizeof(htab_t));
     if (tab == NULL) {
-        error_exit("Neuspěsná alokace tabulky v htab_init",0);
+        warning_msg("Neuspěsná alokace tabulky v htab_init",0);
+        return NULL;
     }
     tab->arr_size = n;
     tab->arr_ptr = calloc(n, sizeof(htab_item_t*));
     if (tab->arr_ptr == NULL) {
         free(tab);
-        error_exit("Neuspěsná alokace arr_ptr v htab_init",0);
+        warning_msg("Neuspěsná alokace arr_ptr v htab_init",0);
+        return NULL;
     }   
     return tab;
 }
