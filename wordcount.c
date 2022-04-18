80ec7bc42f05e55338ed3bf870377f3d0fc7c90f
Robot7769 <jan.robot7@gmail.com>
2022-04-18 13:39:56 +0200

    wordcount + debugg výpisy

--- wordcount.c
+++ wordcount.c
@@ -7,3 +7,28 @@
  * @copyright Copyright (c) 2022
  * 
  */
+
+#include "htab.h"
+#include "io.c"
+
+void print_htab(htab_pair_t *data) {
+    printf("%s\t%d\n",data->key,data->value);
+}
+
+int main() {
+    printf("START\n");
+    htab_t *tab = htab_init(150);
+    printf("Htab init\n");
+    FILE *file = stdin;
+    char word[127];  
+    printf("read_word\n");
+    while (read_word(word,127,file) != EOF) {
+        htab_pair_t *data = htab_lookup_add(tab,word);
+        (data->value)++;
+        printf("data val ++\n");
+    }
+    printf("write\n");
+    htab_for_each(tab, &print_htab);
+    printf("KONEC\n");
+    return 0;
+}
