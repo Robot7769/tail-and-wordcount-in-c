60e480b7b097b9d279aa6bba5643d780251502f6
Robot7769 <jan.robot7@gmail.com>
2022-04-18 14:45:58 +0200

    oprava

--- htab_clear.c
+++ htab_clear.c
@@ -24,24 +24,26 @@ void htab_clear(htab_t * t) {
     size_t j = 0;
     for (size_t i = 0; i < t->arr_size; i++) {
         htab_item_t *tmp = t->arr_ptr[i];
-        htab_item_t *tmp_prev = tmp;
-        if (tmp->next != NULL) {
-            tmp = tmp->next;
-            for (; j < t->size; j++) {
-                //free(tmp_prev->data->key);
-                free(tmp_prev->data);
-                free(tmp_prev);
-                tmp_prev = tmp;
-                if (tmp->next != NULL) {
-                    tmp = tmp->next;
-                } else {
-                    break;
+        if (tmp != NULL) {
+            htab_item_t *tmp_prev = tmp;
+            if (tmp->next != NULL) {
+                tmp = tmp->next;
+                for (; j < t->size; j++) {
+                    //free(tmp_prev->data->key);
+                    free(tmp_prev->data);
+                    free(tmp_prev);
+                    tmp_prev = tmp;
+                    if (tmp->next != NULL) {
+                        tmp = tmp->next;
+                    } else {
+                        break;
+                    }
                 }
-            }
-        } 
-        //free(tmp->data->key);
-        free(tmp->data);
-        free(tmp);
+            } 
+            //free(tmp->data->key);
+            free(tmp->data);
+            free(tmp);
+        }
     }
     free(t->arr_ptr);
     t->size = 0;
