39b1ee792023cd71b196bd2f7420f8f046fac832
Robot7769 <jan.robot7@gmail.com>
2022-04-18 22:52:05 +0200

    přetypování - není potřeba

--- htab_init.c
+++ htab_init.c
@@ -13,13 +13,13 @@
 #include "error.h"
 
 htab_t *htab_init(size_t n) {
-    htab_t *tab = calloc(1, sizeof(htab_t));
+    htab_t *tab = (htab_t *) calloc(1, sizeof(htab_t));
     if (tab == NULL) {
         warning_msg("Neuspěsná alokace tabulky v htab_init",0);
         return NULL;
     }
     tab->arr_size = n;
-    tab->arr_ptr = calloc(n, sizeof(htab_item_t*));     //? co se stane když zavolám htab_init(0); ????!!!!
+    tab->arr_ptr = (htab_item_t **) calloc(n, sizeof(htab_item_t*));     //? co se stane když zavolám htab_init(0); ????!!!!
     if (tab->arr_ptr == NULL) {
         free(tab);
         warning_msg("Neuspěsná alokace arr_ptr v htab_init",0);
