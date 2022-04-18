2d236825e0d40d90ca832eb96794deebad468327
Robot7769 <jan.robot7@gmail.com>
2022-04-18 13:39:16 +0200

    TODO!! Deklatace funkce, Dodělat

---
+++ htab_resize.c
@@ -0,0 +1,18 @@
+/**
+ * @file htab_resize.c
+ * @author Jan Škrabal (xskrab12)-FIT
+ * @brief IJC-DU2 Tabulka
+ * @date 2022-03-23
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "htab_struct.h"
+#include "error.h"
+
+void htab_resize(htab_t *t, size_t newn) {
+    //TODO ^^^^^
+    warning_msg("TODO",0); //!Dodělat
+    return;
+}
