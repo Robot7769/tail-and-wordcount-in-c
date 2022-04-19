5beab17a24eeb2cb8ff91d1739355c9da386de7b
Robot7769 <jan.robot7@gmail.com>
2022-04-19 15:37:56 +0200

    začátek resize

--- htab_resize.c
+++ htab_resize.c
@@ -12,6 +12,42 @@
 #include "error.h"
 
 void htab_resize(htab_t *t, size_t newn) {
+    if (t == NULL) {
+        warning_msg("htab_t * t je NULL, modul htab_resize",0);
+        return NULL;
+    }
+    if (t->arr_size == 0 || t->arr_ptr == NULL) {
+        return;
+    }
+    htab_t *new = htab_init(newn);
+    size_t j = 0;
+    for (size_t i = 0; i < t->arr_size; i++) { //! potřeba opravit, pomocí klíče nemusím procházet celou strukturu
+        htab_item_t *tmp = t->arr_ptr[i];
+        //printf("each-1\n");
+        if (t->arr_ptr[i] != NULL) {
+            //printf(" --each-- ");
+            for (; j < t->size; j++) {
+                //printf("each00\n");
+                if (tmp->data != NULL) {
+                    //printf("each01\n");
+                    htab_pair_t *new_data = htab_lookup_add(new,tmp->data->key);
+                    //printf("ptr: %p -data\n", (void *) data);
+                    //printf("\n%s slovo\n", data->key);
+                    new_data->value += 1;
+                }
+                if (tmp->next != NULL) {
+                    tmp = tmp->next;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    //zavolta clear 
+    //smazeat **na arr_ptr
+    //
+
+
     //TODO ^^^^^
     warning_msg("TODO",0); //!Dodělat
     return;
