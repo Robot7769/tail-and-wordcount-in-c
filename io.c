904b5da72b9d7ab48759268184d7ccdd687d6a3d
Robot7769 <jan.robot7@gmail.com>
2022-04-19 15:34:18 +0200

    oprav io.c vynechává bíle znaky

--- io.c
+++ io.c
@@ -17,10 +17,16 @@ bool err_long_word = false;
 int read_word(char *s, int max, FILE *f) {
     //printf("čtení slova start\n");
     //char word[max];
+    int znak;
     for (int i = 0; i < max-1; i++) {
         //printf("for\n");
-        int znak = fgetc(f);
+        znak = fgetc(f);
         //printf("znak_cislo: %d\n",znak);
+        if (i == 0) {
+            while ((char)znak == '\0' || (char)znak == ' ' || (char)znak == '\n' || (char)znak == '\t'){
+                znak = fgetc(f);    
+            }
+        }
         if ((char)znak == '\0' || (char)znak == ' ' || (char)znak == '\n' || (char)znak == '\t') {
             //s[i+1] = '\0';
             //printf("vracím slovo-konec: %s\n", s);
@@ -38,15 +44,17 @@ int read_word(char *s, int max, FILE *f) {
         
     }
     //s[max] = '\0';
-    printf("vracím slovo-chyba: %s\n", s);
+    //printf("vracím slovo-chyba: %s\n", s);
     if (!err_long_word) {
         err_long_word = true;
         warning_msg("slovo je moc dlouhé\n",0);
     }
-
+    while ((char)znak != '\0' && (char)znak != ' ' && (char)znak != '\n' && (char)znak != '\t'){
+                znak = fgetc(f);    
+    }
     //s = word;
     //!error delší slovo
-    return max; //? EOF
+    return max;
 
 
     //TODO
