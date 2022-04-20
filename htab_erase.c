db2e9e0ce0be7417e885ac4b6254c9a125c07eb7
Robot7769 <jan.robot7@gmail.com>
2022-04-20 15:34:04 +0200

    add stdlib to erase

--- htab_erase.c
+++ htab_erase.c
@@ -10,6 +10,7 @@
 
 #include "htab_struct.h"
 #include "error.h"
+#include <stdlib.h>
 
 bool htab_erase(htab_t * t, htab_key_t key) {
     if (t == NULL) {
@@ -40,44 +41,6 @@ bool htab_erase(htab_t * t, htab_key_t key) {
         tmp  = tmp->next;
 
     }
-    /*
-    size_t j = 1;
-    for (size_t i = 0; i < t->arr_size; i++) { //! potřeba opravit, pomocí klíče nemusím procházet celou strukturu
-        if (tmp->data->key == key) {
-            t->arr_ptr[i] = tmp->next;
-            //free(tmp->data->key);
-            free(tmp->data);
-            t->arr_ptr[i] = tmp->next;
-            free(tmp);
-            (t->size)--;
-            if (t->size / t->arr_size < AVG_LEN_MIN) {
-                htab_resize(t,(size_t) (t->size/2) + (t->size%2));
-            } 
-            return true;
-        }
-        htab_item_t *tmp_prev = tmp;
-        if (tmp->next != NULL) {
-            tmp = tmp->next;
-            for (; j < t->size; j++) {
-                if (tmp->data->key == key) {
-                    tmp_prev = tmp->next;
-                    //free(tmp->data->key);
-                    free(tmp->data);
-                    tmp_prev = tmp->next;
-                    free(tmp);
-                    (t->size)--;
-                    if (t->size / t->arr_size < AVG_LEN_MIN) {
-                        htab_resize(t,(size_t) (t->size/2) + (t->size%2));
-                    }
-                    return true;
-                }
-                if (tmp->next != NULL) {
-                    tmp = tmp->next;
-                } else {
-                    break;
-                }
-            }
-        }
-    }*/
+
     return false;
 }
